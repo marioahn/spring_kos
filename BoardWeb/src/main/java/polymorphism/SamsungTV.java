6c6cebf5c19231d68e7265104d873d1f25b8b4d1
unknown <marioahn@naver.com>
2024-05-08 13:01:48 +0900

    spring-container-bean configuration-finish

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,19 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+	
+	public SamsungTV() {
+		System.out.println("삼성 TV 객체 생성 완료");
+	}
+	// bean-confirgurations파일에 samsungTV bean에 init,destroyMethod넣어주려면 여기서 만들어야겠지ㅇㅇ
+	public void initMethod() {
+		System.out.println("객체 초.기화 작업 완료");
+	}
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전 처리할 명령을 지정하는 부분");
+	}
+	
+	
 	@Override
 	public void powerOn() {
 		System.out.println("삼성티비 - 전원 켠다");
---
+++ BoardWeb/src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class SonySpeaker {
+	public SonySpeaker() {
+		System.out.println("===> SonySpeaker 객체 생성");
+	}
+	public void volumeUp() {
+		System.out.println("Sony볼륨 업");
+	}
+	public void volumeDown() {
+		System.out.println("Sony볼륨 다운");
+	}
+}
