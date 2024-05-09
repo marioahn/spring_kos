202367d04393e8594a976bddecfeb990c2da84cb
unknown <marioahn@naver.com>
2024-05-09 18:55:38 +0900

    CRUD-base code write

--- BoardWeb/src/main/java/com/springbook/biz/board/BoardService.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/BoardService.java
@@ -1,5 +1,23 @@
 package com.springbook.biz.board;
 
-public class BoardService {
+import java.util.List;
 
+// CRUD기능 메소드 구현
+public interface BoardService {
+	
+	// 1. 등록
+	void insertBoard(BoardVO vo);
+	
+	// 2. 수정
+	void updateBoard(BoardVO vo);
+
+	// 3. 삭제
+	void deleteBoard(BoardVO vo);
+
+	// 4. 글 상세 조회
+	BoardVO getBoard(BoardVO vo);
+
+	// 5. 글 전체 조회
+	List<BoardVO> getBoardList(BoardVO vo);	
+	
 }
--- BoardWeb/src/main/java/com/springbook/biz/common/JDBCUtil.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/JDBCUtil.java
@@ -1,5 +1,73 @@
 package com.springbook.biz.common;
 
-public class JDBCUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class JDBCUtil {
+	public static Connection getConneciton() { // Connection타입
+		try {
+			Class.forName("org.h2.Driver");
+			return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void close(PreparedStatement stmt, Connection conn) {
+		if (stmt != null) {
+			try {
+				if (!stmt.isClosed()) stmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				stmt = null;
+			}
+		}
+		
+		if (conn != null) {
+			try {
+				if (!conn.isClosed()) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn = null;
+			}
+		}
+	}
+	
+	// close함수 오버.로딩!
+	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+		if (rs != null) {
+			try {
+				if (!rs.isClosed()) rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				rs = null;
+			}
+		}
+		
+		if (stmt != null) {
+			try {
+				if (!stmt.isClosed()) stmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				stmt = null;
+			}
+		}
+		
+		if (conn != null) {
+			try {
+				if (!conn.isClosed()) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn = null;
+			}
+		}
+	}
 }
